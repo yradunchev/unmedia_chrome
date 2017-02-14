8813539f5fb463e907b2d9cb0e4f664d801a2cfe
Yordan Radunchev <yordan@radunchev.com>
2017-02-14 08:36:03 +0200

    Typo.

--- alert.js
+++ alert.js
@@ -4,7 +4,7 @@ var striped_lines_box = document.createElement('div');
 striped_lines_box.style.cssText = "background-image: url(https://i.imgur.com/m5Wjo7h.png); height: 120px;width: 100%;position: fixed; z-index: 99999; opacity: 0.7;";
 
 var inner_text = document.createElement('span');
-inner_text.textContent = "Този сайт разпространява подвеждаща или невярна информаця!";
+inner_text.textContent = "Този сайт разпространява подвеждаща или невярна информация!";
 inner_text.style.cssText = "font-family: 'Lucida Console', Monaco, monospace; color: white; margin: auto; background: black; display: block; height: 120px; width: 500px; text-align: center; font-size: 24px;";
 
 striped_lines_box.appendChild(inner_text);
